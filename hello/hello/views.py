4888058e3062677e0afecbdb4f2b5c1987963282
ayedaren <1783580635@qq.com>
2020-01-31 10:39:10 +0800

    精简业务代码

--- hello/hello/views.py
+++ hello/hello/views.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
-from django.http import JsonResponse
-from ehall.login import login, jw_login, clear_Data
+from ehall.login import login, jw_login, clear_Data, jw_get, jw_get_photo
 from django.views.decorators.csrf import csrf_exempt
 import json
 import time
 
-cardMoney_URl = "http://ehall.cidp.edu.cn/publicapp/sys/myyktzd/api/getConsumeByRange.do?startY=2019&startM=7&endY=2019&endM=12&isOvered=0&_=1575360061774"
-netCost_URL = "http://ehall.cidp.edu.cn/jsonp/personalRemind/getViewDataDetail.do?wid=950af967525b4bffb3db654958683e3d&mailAccount=&_=1575341578516"
-course_URL = "http://ehall.cidp.edu.cn/publicapp/sys/pubwdkbapp/api/getMyTimeTableList.do?_=1575360061762"
-info_URL = "http://ehall.cidp.edu.cn/jsonp/userDesktopInfo.json?type=&_=1575365028115"
+cardMoney_URL = "http://ehall.cidp.edu.cn/publicapp/sys/myyktzd/api/getConsumeByRange.do?startY=2019&startM=7&endY=2019&endM=12&isOvered=0&_="
+netCost_URL = "http://ehall.cidp.edu.cn/jsonp/personalRemind/getViewDataDetail.do?wid=950af967525b4bffb3db654958683e3d&mailAccount=&_="
+course_URL = "http://ehall.cidp.edu.cn/publicapp/sys/pubwdkbapp/api/getMyTimeTableList.do?_="
+info_URL = "http://ehall.cidp.edu.cn/jsonp/userDesktopInfo.json?type=&_="
+specialApi = ["grade", "getInfo"]
 
 
 def display_time(fun):
@@ -22,31 +22,11 @@ def display_time(fun):
     return deco
 
 
-def doFunction(apiname,username,password):
-    url = eval(apiname + "_URL")
-    res=login(url,username,password)
+def doFunction(apiname, username, password):
+    re = int(round(time.time() * 1000))
+    url = eval(apiname + "_URL")+str(re)
+    res = login(url, username, password)
     return res
-
-
-
-def cardMoney(username, password):
-    url = CARD_URL
-    res = login(url=url, username=username, password=password)
-    return res
-
-
-
-def netCost(username, password):
-    url = netCost_URL
-    res = login(url=url, username=username,  password=password)
-    return res
-
-
-def course(username, password):
-    url = course_URL
-    res = login(url=url, username=username, password=password)
-    return res
-
 
 
 def grade(username, password):
@@ -58,18 +38,32 @@ def grade(username, password):
         return res
 
 
-def info(username, password):
-    url = info_URL
-    res = login(url=url, username=username, password=password)
-    return res
+def getInfo(username, password):
+    res = jw_get(username=username, password=password)
+    if type(res) is not int:
+        return res
+    else:
+        return res
 
+@csrf_exempt
+def getPhoto(request):
+    username = json.loads(request.body)["username"]
+    password = json.loads(request.body)["password"]
+    res = jw_get_photo(username, password)
+    if type(res) is not int:
+        return HttpResponse(res, content_type="image/jpeg")
+    else:
+        return HttpResponse(status=res)
 
 @csrf_exempt
 def api(request):
     username = json.loads(request.body)["username"]
     password = json.loads(request.body)["password"]
     apiname = json.loads(request.body)["apiname"]
-    res = eval(apiname)(username, password)
+    if apiname in specialApi:
+        res = eval(apiname)(username, password)
+    else:
+        res = doFunction(apiname, username, password)
     if type(res) is not int:
         return HttpResponse(json.dumps(res, ensure_ascii=False), content_type="application/json,charset=utf-8")
     else:
